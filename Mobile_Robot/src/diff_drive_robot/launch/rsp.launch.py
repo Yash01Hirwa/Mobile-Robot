1852c6a481fdb865c5c0745b73332e44eb05bfd7
Yash01Hirwa <yashhirwa01@gmail.com>
2025-10-14 14:09:16 +0530

    URDF Creation

---
+++ Mobile_Robot/src/diff_drive_robot/launch/rsp.launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    pkg_path = os.path.join(get_package_share_directory('diff_drive_robot'))
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    parameters = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[parameters]
+    )
+    
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+        ),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        node_robot_state_publisher
+    ])
+    
